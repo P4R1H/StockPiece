b0c765f8b462fb9b57e0dc2075161c42f40bf600
P4R1H <76397616+P4R1H@users.noreply.github.com>
2025-03-11 04:41:50 +0530

    Removed vercel packages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { SpeedInsights } from "@vercel/speed-insights/react"
-import { Analytics } from '@vercel/analytics/react'
 
 import './index.css'
 import App from './App.tsx'
@@ -13,8 +11,6 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <SpeedInsights />
-      <Analytics />
     </QueryClientProvider>
   </StrictMode>,
 )
